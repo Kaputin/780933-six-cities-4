a1c3732e6f08379e572f73ce7b1ae63932568310
Kaputin <kaputinevgeny@gmail.com>
2020-07-22 00:49:49 +0300

    Сделал задание Личный проект: таинственные подробности

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,47 +3,82 @@ import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {ActionCreator} from "../../reducer.js";
 import {Main} from "../main/main.jsx";
+import {Property} from "../property/property.jsx";
 import {OfferPropTypes, CityPropTypes} from "../../propTypes.js";
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
-const offerTitleHandler = () => {};
+// export const App = (props) => { // потом уберу оставил, чтобы не забыть
+//   const {
+//     loadCities,
+//     cities,
+//     selectedOffers,
+//     selectedCity,
+//     selectedCoordinates,
+//     onCityTitleClick,
+//   } = props;
+
+// componentDidMount() {
+//   this.prop.loadCities();
+// }
+
+// App.defaultProps = { // потом уберу оставил, чтобы не забыть
+//   selectedCity: {
+//     id: 1,
+//     title: `Amsterdam`,
+//     coordinates: [52.38333, 4.9]
+//   }
+// };
 
 export class App extends PureComponent {
   constructor(props) {
     super(props);
+    this.offerTitleClickHandler = this.offerTitleClickHandler.bind(this);
+
+    this.state = {
+      currentOffer: null
+    };
   }
 
+  offerTitleClickHandler(offer) {
+    this.setState({
+      currentOffer: offer
+    });
+  }
 
-  // export const App = (props) => { // потом уберу оставил, чтобы не забыть
-  //   const {
-  //     loadCities,
-  //     cities,
-  //     selectedOffers,
-  //     selectedCity,
-  //     selectedCoordinates,
-  //     onCityTitleClick,
-  //   } = props;
-
-  // componentDidMount() {
-  //   this.prop.loadCities();
-  // }
-
-  render() {
+  _renderApp() {
     const {
-      // loadCities, // потом уберу оставил, чтобы не забыть
       cities,
       selectedOffers,
       selectedCity,
       onCityTitleClick,
     } = this.props;
 
+    if (!this.state.currentOffer) {
+      return (
+        <Main
+          offersCount={selectedOffers.length}
+          offers={selectedOffers}
+          cities={cities}
+          selectedCity={selectedCity}
+          onCityTitleClick={onCityTitleClick}
+          onOfferTitleClick={this.offerTitleClickHandler} />
+      );
+    }
+    return <Property offer={this.state.currentOffer} />;
+  }
+
+  render() {
     return (
-      <Main
-        offersCount={selectedOffers.length}
-        offers={selectedOffers}
-        cities={cities}
-        selectedCity={selectedCity}
-        onCityTitleClick={onCityTitleClick}
-        onOfferTitleClick={offerTitleHandler} />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderApp()}
+          </Route>
+          <Route exact path="/dev-component">
+            <Property offer={this.props.selectedOffers[0]} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
@@ -54,14 +89,6 @@ App.propTypes = {
   selectedCity: CityPropTypes,
   onCityTitleClick: PropTypes.func.isRequired,
 };
-
-// App.defaultProps = { // потом уберу оставил, чтобы не забыть
-//   selectedCity: {
-//     id: 1,
-//     title: `Amsterdam`,
-//     coordinates: [52.38333, 4.9]
-//   }
-// };
 
 const mapStateToProps = (state) => ({
   cities: state.cities,
